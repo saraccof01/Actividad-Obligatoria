99a921e5572512d5d927cfc352ab90bc0161e29f
Fiorella Saracco <saraccof01@gmail.com>
2022-05-23 01:46:09 -0300

    cambio de caracteres

--- control.js
+++ control.js
@@ -5,13 +5,13 @@ var clave=document.getElementById("clave");
 form.onsubmit=function(e){
     if(usuario.value===""||clave.value===""){
         e.preventDefault();
-        alert("Completá ambos datos!")
+        alert("Completa ambos datos!")
     }
 }
 function usuarioArroba(e){
     if (usuario.value!="usu@rio"){
         e.preventDefault();
-        alert("Recordá utilizar un '@' en el usuario!")
+        alert("Recorda utilizar un '@' en el usuario!")
     }
 }
 form.addEventListener('submit',usuarioArroba);
